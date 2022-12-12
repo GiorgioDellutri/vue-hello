e439edd9585aea7f9fe675962f3aea87d7ffa1c6
GiorgioDellutri <giorgiodll93@gmail.com>
2022-12-12 17:07:04 +0100

    img logo

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const {createApp} = Vue;
 createApp({
     data() {
         return{
-            message: 'Vue js is awesome!'
+            message: 'Vue js is awesome!',
+            cssClass: 'blueviolet',
+            imgLogo: '.././img/logo.png'
         }
     }
 }).mount('#root')
